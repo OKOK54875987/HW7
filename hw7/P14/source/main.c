29f67d7795b8165b317e5994ac9068e55a3776e0
zhengyuan <okok54875987@gmail.com>
2021-12-23 21:43:19 +0800

    hw7

---
+++ hw7/P14/source/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	FILE* fptr;
+	char ch;
+	int count = 0;
+
+	fptr = fopen("C://Users/user/OneDrive/桌面/ch7/hw7/hw7/P14/welcome.txt", "r"); //P14中檔案路徑
+	if (fptr != NULL)
+	{
+		while ((ch = getc(fptr)) != EOF)
+		{
+			printf("%c", ch);
+			count++;
+		}
+		fclose(fptr);
+		printf("\n總共有%d個字元\n", count);
+	}
+	else
+		printf("檔案開啟失敗!\n");
+	system("pause");
+	return 0;
+}
